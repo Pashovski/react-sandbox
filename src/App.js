c1010df50f4455d2feecf036bf9c3d29fc987f32
Pashovski <pashdoctor@gmail.com>
2022-03-30 12:10:58 -0400

    useMemo example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import UseMemoExample from './components/UseMemoExample'
 import UseRefExample3 from './components/UseRefExample3'
 function App() {
   return (
     <div className='container mt-5'>
-      <UseRefExample3 />
+      <UseMemoExample />
     </div>
   )
 }
